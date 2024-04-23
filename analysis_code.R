abdcf651b0a6f1301eda97fbb4d3e674ba0fb989
Grant Mitchell <grantam@byu.edu>
2024-04-22 20:56:27 -0600

    finished analysis

--- analysis_code.R
+++ analysis_code.R
@@ -11,7 +11,10 @@ library(vdemdata)
 library(lme4)
 library(readxl)
 library(rstanarm)
-
+library(Matrix)
+library(zoo)
+library(plm)
+library(reghelper)
 #### functions
 
 neg_binom <- function(bi_var) {
@@ -45,32 +48,24 @@ impute <- function(df, var) {
 #### Clean data
 
 data <- vdem %>%
-  mutate(turnover_test = ifelse(v2elturnhog == 0, 0, 1), COWcode = as.factor(COWcode), election = ifelse(is.na(v2eltype_0), 0, 1), seatshare = ifelse(is.na(v2ellostsl), 100, v2ellostsl)) %>%
+  mutate(turnover_test = ifelse(v2elturnhog == 0, 0, 1), COWcode = as.factor(COWcode), election = ifelse(is.na(v2eltype_0), 0, 1)) %>%
   replace_na(replace = list(turnover_test = 0, v2elturnhog = 0)) %>%
   mutate(v2elturnhog = as.factor(v2elturnhog), fix = as.factor(country_name)) %>%
-  select(turnover_test, v2x_polyarchy, v2x_libdem, election, COWcode, v2elturnhog, year, fix, seatshare, v2elsuffrage, e_p_polity) %>%
+  select(turnover_test, v2x_api, v2x_libdem, election, COWcode, v2elturnhog, year, fix, v2ellostsl, v2elsuffrage, e_p_polity, v2expathhg, v2ex_elechog, v2ex_elechos, v2exhoshog, ifs = country_text_id, v2expathhg, v2expathhs, v2x_freexp_altinf, v2xel_frefair) %>%
   group_by(COWcode) %>%
-  mutate(last_elect = log(neg_binom(election) + 1), var_elect = var(last_elect))
+  mutate(last_elect = log(neg_binom(election) + 1), var_elect = var(last_elect), exec_chose = ifelse(v2exhoshog == 1, v2expathhs, v2expathhg-1), parli = ifelse(exec_chose == 6, 1, 0))
 
-lexical <- read_excel("C:/Users/ochoc/Downloads/LIED_6.5.xlsx") %>%
+
+lexical <- read_excel("C:/Users/grantam/Downloads/LIED_6.5.xlsx") %>%
   rename(COWcode = cow) %>%
   mutate(COWcode = as.factor(COWcode))
 
 data <- left_join(data, lexical, by = c("year", "COWcode")) %>%
   mutate(lexical_index = (lexical_index/6), lexical_index_plus = (lexical_index_plus/7))
 
-parties <- vparty %>%
-  group_by(COWcode, year) %>%
-  summarize(number_of_parties = n()) %>%
-  mutate(COWcode = as.factor(COWcode))
-
-data <- left_join(data, 
-                  parties, 
-                  by = c("year", "COWcode"))
-
 data <- cbind(data, 
               datawizard::demean(data, 
-                                 select = c("last_elect","seatshare", "executive_elections", "legislative_elections", "multi-party_legislative_elections", "turnover_event", "number_of_parties", "competitive_elections", "v2elsuffrage", "e_p_polity", "political_liberties"),
+                                 select = c("last_elect","v2ellostsl", "executive_elections", "legislative_elections", "multi-party_legislative_elections", "turnover_event", "competitive_elections", "v2elsuffrage", "e_p_polity", "political_liberties", "parli", "v2exhoshog", "v2x_freexp_altinf", "v2xel_frefair"),
                                  group = "COWcode"))
 
 data <- data %>%
@@ -81,7 +76,6 @@ data <- data %>%
       "legislative_elections_within", 
       "executive_elections_within", 
       "multi.party_legislative_elections_within", 
-      "seatshare_within", 
       "last_elect_within", 
       "competitive_elections_within",
       "v2elsuffrage_within",
@@ -89,12 +83,19 @@ data <- data %>%
       "turnover_event_between", 
       "legislative_elections_between", 
       "executive_elections_between", 
-      "multi.party_legislative_elections_between", 
-      "seatshare_between", 
+      "multi.party_legislative_elections_between",
       "last_elect_between",
       "competitive_elections_between",
       "v2elsuffrage_between",
-      "political_liberties_between"
+      "political_liberties_between",
+      "parli_between",
+      "parli_within",
+      "v2exhoshog_within",
+      "v2exhoshog_between",
+      "v2x_freexp_altinf_within",
+      "v2x_freexp_altinf_between",
+      "v2xel_frefair_within",
+      "v2xel_frefair_between"
     )),
     ~ (.-mean(., na.rm = TRUE)) / (2*(sd(., na.rm = TRUE)))
   ))
@@ -102,7 +103,7 @@ data <- data %>%
 #### Exploratory viz
 
 ggplot(data = data) +
-  geom_smooth(aes(x = year, y = v2x_polyarchy), color = "grey40", linewidth = 2, se = F) +
+  geom_smooth(aes(x = year, y = v2x_api), color = "grey40", linewidth = 2, se = F) +
   geom_smooth(aes(x = year, y = lexical_index_plus), color = "black", linewidth = 2, se = F) +
   labs(title = "Lexical Index vs. V-Dem Polyarchy Index", x = "Year", y = "Average Democracy in the World") +
   theme_bw() + 
@@ -113,21 +114,38 @@ ggplot(data = data) +
 
 #### Model
 
+##### Standard
+
+m0 <- lmer(v2x_api ~ 
+             turnover_event + 
+             legislative_elections + 
+             executive_elections + 
+             `multi-party_legislative_elections` + 
+             last_elect + 
+             competitive_elections +
+             v2elsuffrage +
+             political_liberties +
+             (1|COWcode),
+           data = data)
+
+summary(m0)
+
+ICC(m0)
 
 
-m1 <- lmer(v2x_polyarchy ~ 
+##### Standard with Between effects
+
+m1 <- lmer(v2x_api ~ 
              turnover_event_within + 
              legislative_elections_within + 
              executive_elections_within + 
              multi.party_legislative_elections_within + 
-             seatshare_within + 
              last_elect_within + 
              competitive_elections_within +
              turnover_event_between + 
              legislative_elections_between + 
              executive_elections_between + 
              multi.party_legislative_elections_between + 
-             seatshare_between + 
              last_elect_between + 
              competitive_elections_between +
              v2elsuffrage_between +
@@ -137,21 +155,74 @@ m1 <- lmer(v2x_polyarchy ~
              (1|COWcode),
            data = data)
 
-m2 <- lmer(v2x_polyarchy ~ 
-             turnover_event + 
-             legislative_elections + 
-             executive_elections +  `multi-party_legislative_elections` + 
-             seatshare + 
-             last_elect + 
-             competitive_elections +
-             v2elsuffrage +
-             political_liberties +
+summary(m1)
+
+ICC(m1)
+
+##### Add Presidentialism with Between effects
+
+m2 <- lmer(v2x_api ~ 
+             turnover_event_within + 
+             legislative_elections_within + 
+             executive_elections_within + 
+             multi.party_legislative_elections_within + 
+             last_elect_within + 
+             competitive_elections_within +
+             turnover_event_between + 
+             legislative_elections_between + 
+             executive_elections_between + 
+             multi.party_legislative_elections_between + 
+             last_elect_between + 
+             competitive_elections_between +
+             v2elsuffrage_between +
+             v2elsuffrage_within +
+             political_liberties_within +
+             political_liberties_between +
+             v2exhoshog_within +
+             v2exhoshog_between +
+             parli_between +
+             parli_within +
              (1|COWcode),
            data = data)
 
-anova(m1, m2)
+summary(m2)
 
-names <- c("Intercept", "Turnover Within", "Legislative Elections Within", "Executive Elections Within", "Multi-Party Elections Within", "Seatshare Within", "Years Since Election Within", "Uncertainty Within", "Turnover Between", "Legislative Elections Between", "Executive Elections Between", "Multi-Party Elections Between", "Seatshare Between", "Years Since Election Between", "Uncertainty Between", "Suffrage Between", "Suffrage Within", "Political Liberties Within", "Political Liberties Between")
+ICC(m2)
+
+##### Add Presidentialism and media freedom with Between effects
+
+m3 <- lmer(v2x_api ~ 
+             turnover_event_within + 
+             legislative_elections_within + 
+             executive_elections_within + 
+             multi.party_legislative_elections_within + 
+             last_elect_within + 
+             v2xel_frefair_within +
+             turnover_event_between + 
+             legislative_elections_between + 
+             executive_elections_between + 
+             multi.party_legislative_elections_between + 
+             last_elect_between + 
+             v2xel_frefair_between +
+             v2elsuffrage_between +
+             v2elsuffrage_within +
+             v2exhoshog_within +
+             v2exhoshog_between +
+             parli_between +
+             parli_within +
+             v2x_freexp_altinf_within +
+             v2x_freexp_altinf_between +
+             (1|COWcode),
+           data = data)
+
+summary(m3)
+
+ICC(m3)
+
+
+anova(m0, m1)
+
+names <- c("Intercept", "Turnover Within", "Legislative Elections Within", "Executive Elections Within", "Multi-Party Elections Within", "Years Since Election Within", "Uncertainty Within", "Turnover Between", "Legislative Elections Between", "Executive Elections Between", "Multi-Party Elections Between", "Years Since Election Between", "Uncertainty Between", "Suffrage Between", "Suffrage Within", "Political Liberties Within", "Political Liberties Between")
 
 coef.plot <- summary(m1)[10]
 coef.plot2 <- coef.plot[["coefficients"]]
@@ -177,25 +248,27 @@ ggplot(data = viz_data) +
 
 #### Stan
 
-options(mc.cores = 6)
+options(mc.cores = parallel::detectCores())
 
-stan1 <- stan_lmer(v2x_polyarchy ~ 
+stan1 <- stan_lmer(v2x_api ~ 
                      turnover_event_within + 
                      legislative_elections_within + 
                      executive_elections_within + 
                      multi.party_legislative_elections_within + 
-                     seatshare_within + 
                      last_elect_within + 
                      competitive_elections_within +
                      turnover_event_between + 
                      legislative_elections_between + 
                      executive_elections_between + 
                      multi.party_legislative_elections_between + 
-                     seatshare_between + 
                      last_elect_between + 
                      competitive_elections_between +
+                     v2elsuffrage_between +
+                     v2elsuffrage_within +
+                     political_liberties_within +
+                     political_liberties_between +
                      (1|COWcode),
-                   data = data, algorithm = "sampling")
+                   data = data, algorithm = "sampling", cores = 12)
 
 sims <- as.matrix(stan1)
 # draws for overall mean
@@ -220,7 +293,85 @@ a_df <- data.frame(a_mean, a_sd, a_quant)
 a_df <- a_df[order(a_df$a_mean), ]
 a_df$a_rank <- c(1 : dim(a_df)[1])
 a_df <- a_df %>%
-  mutate(sig = ifelse((Q2.5 > 0.2602782 & Q97.5 > 0.2602782 | Q2.5 < 0.2602782 & Q97.5 < 0.2602782), T, F))
+  mutate(sig = ifelse((Q2.5 > mean(a_df$a_mean) & Q97.5 > mean(a_df$a_mean) | Q2.5 < mean(a_df$a_mean) & Q97.5 < mean(a_df$a_mean)), T, F))
+
+# a vector of country rank 
+
+# Caterpillar plot
+# Plot county-level alphas's posterior mean and 95% credible interval
+ggplot(data = a_df, 
+       aes(x = a_rank, 
+           y = a_mean, color = sig)) +
+  geom_pointrange(aes(ymin = Q2.5, 
+                      ymax = Q97.5,
+                      shape = sig), size = 1, linewidth = 2) + 
+  geom_hline(yintercept = mean(a_df$a_mean), 
+             size = 0.5, 
+             col = "darkblue") + 
+  scale_x_continuous(breaks = seq(from = 0, 
+                                  to = 196, 
+                                  by = 10)) + 
+  scale_color_manual(values = c("darkblue", "darkgreen")) +
+  theme_bw() +
+  labs(y = "Country-Intercept", x = "Rank", title = "Country Intercepts with 95% CI") +
+  annotate("text", label = "Fixed Intercept", x = 185, y= mean(a_df$a_mean)-.03, size = 10, color = "darkblue") +
+  theme(legend.position = "none") +
+  theme(plot.title = element_text(size = 30), axis.title = element_text(size = 20), axis.text = element_text(size = 20), plot.caption = element_text(size = 15))
+
+
+#### Model
+
+
+options(mc.cores = parallel::detectCores())
+
+stan2 <- stan_lmer(v2x_api ~ 
+                     turnover_event_within + 
+                     legislative_elections_within + 
+                     executive_elections_within + 
+                     multi.party_legislative_elections_within + 
+                     last_elect_within + 
+                     competitive_elections_within +
+                     turnover_event_between + 
+                     legislative_elections_between + 
+                     executive_elections_between + 
+                     multi.party_legislative_elections_between + 
+                     last_elect_between + 
+                     competitive_elections_between +
+                     v2elsuffrage_between +
+                     v2elsuffrage_within +
+                     political_liberties_within +
+                     political_liberties_between +
+                     v2exhoshog_within +
+                     v2exhoshog_between +
+                     parli_between +
+                     parli_within +
+                     (1|COWcode),
+                   data = data, algorithm = "sampling", cores = 12)
+
+sims <- as.matrix(stan2)
+# draws for overall mean
+mu_a_sims <- as.matrix(sims[, 1])
+# draws for 85 counties county-level random effect
+u_sims <- sims[, 16:211]
+# draws for 85 countys' varying intercepts               
+a_sims <- as.numeric(mu_a_sims) + u_sims          
+
+# Compute mean, SD, median, and 95% credible interval of varying intercepts
+# Posterior mean and SD of each alpha
+a_mean <- apply(a_sims, 2, mean)
+a_sd <- apply(a_sims, 2, sd)
+a_quant <- apply(a_sims, 2, quantile, 
+                 probs = c(0.025, 0.50, 0.975))
+a_quant <- data.frame(t(a_quant))
+names(a_quant) <- c("Q2.5", "Q50", "Q97.5")
+# Combine summary statistics of posterior simulation draws
+a_df <- data.frame(a_mean, a_sd, a_quant)
+
+# Sort dataframe containing an estimated alpha's mean and sd for every county
+a_df <- a_df[order(a_df$a_mean), ]
+a_df$a_rank <- c(1 : dim(a_df)[1])
+a_df <- a_df %>%
+  mutate(sig = ifelse((Q2.5 > mean(a_df$a_mean) & Q97.5 > mean(a_df$a_mean) | Q2.5 < mean(a_df$a_mean) & Q97.5 < mean(a_df$a_mean)), T, F))
 
 # a vector of country rank 
 
@@ -245,4 +396,81 @@ ggplot(data = a_df,
   theme(legend.position = "none") +
   theme(plot.title = element_text(size = 30), axis.title = element_text(size = 20), axis.text = element_text(size = 20), plot.caption = element_text(size = 15))
 
-View(data)
+
+#### Robustness 3
+
+
+
+options(mc.cores = parallel::detectCores())
+
+stan3 <- stan_lmer(v2x_api ~ 
+                     turnover_event_within + 
+                     legislative_elections_within + 
+                     executive_elections_within + 
+                     multi.party_legislative_elections_within + 
+                     last_elect_within + 
+                     v2xel_frefair_within +
+                     turnover_event_between + 
+                     legislative_elections_between + 
+                     executive_elections_between + 
+                     multi.party_legislative_elections_between + 
+                     last_elect_between + 
+                     v2xel_frefair_between +
+                     v2elsuffrage_between +
+                     v2elsuffrage_within +
+                     v2exhoshog_within +
+                     v2exhoshog_between +
+                     parli_between +
+                     parli_within +
+                     v2x_freexp_altinf_within +
+                     v2x_freexp_altinf_between +
+                     (1|COWcode),
+                   data = data, algorithm = "sampling", cores = 12)
+
+sims <- as.matrix(stan2)
+# draws for overall mean
+mu_a_sims <- as.matrix(sims[, 1])
+# draws for 85 counties county-level random effect
+u_sims <- sims[, 16:211]
+# draws for 85 countys' varying intercepts               
+a_sims <- as.numeric(mu_a_sims) + u_sims          
+
+# Compute mean, SD, median, and 95% credible interval of varying intercepts
+# Posterior mean and SD of each alpha
+a_mean <- apply(a_sims, 2, mean)
+a_sd <- apply(a_sims, 2, sd)
+a_quant <- apply(a_sims, 2, quantile, 
+                 probs = c(0.025, 0.50, 0.975))
+a_quant <- data.frame(t(a_quant))
+names(a_quant) <- c("Q2.5", "Q50", "Q97.5")
+# Combine summary statistics of posterior simulation draws
+a_df <- data.frame(a_mean, a_sd, a_quant)
+
+# Sort dataframe containing an estimated alpha's mean and sd for every county
+a_df <- a_df[order(a_df$a_mean), ]
+a_df$a_rank <- c(1 : dim(a_df)[1])
+a_df <- a_df %>%
+  mutate(sig = ifelse((Q2.5 > mean(a_df$a_mean) & Q97.5 > mean(a_df$a_mean) | Q2.5 < mean(a_df$a_mean) & Q97.5 < mean(a_df$a_mean)), T, F))
+
+# a vector of country rank 
+
+# Caterpillar plot
+# Plot county-level alphas's posterior mean and 95% credible interval
+ggplot(data = a_df, 
+       aes(x = a_rank, 
+           y = a_mean, color = sig)) +
+  geom_pointrange(aes(ymin = Q2.5, 
+                      ymax = Q97.5,
+                      shape = sig), size = 1, linewidth = 2) + 
+  geom_hline(yintercept = mean(a_df$a_mean), 
+             size = 0.5, 
+             col = "darkblue") + 
+  scale_x_continuous(breaks = seq(from = 0, 
+                                  to = 196, 
+                                  by = 10)) + 
+  scale_color_manual(values = c("darkblue", "darkgreen")) +
+  theme_bw() +
+  labs(y = "Country-Intercept", x = "Rank", title = "Country Intercepts with 95% CI") +
+  annotate("text", label = "Fixed Intercept", x = 185, y= 0.24, size = 10, color = "darkblue") +
+  theme(legend.position = "none") +
+  theme(plot.title = element_text(size = 30), axis.title = element_text(size = 20), axis.text = element_text(size = 20), plot.caption = element_text(size = 15))
